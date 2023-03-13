eaeeadb7c589ad4c18c642daa0f7e09b4ab6274c
ragozindenis <Ragozin3Dm@yandex.ru>
2023-03-12 21:24:52 -0300

    test

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -51,7 +51,7 @@ class Recipe(models.Model):
     )
     name = models.CharField("Название рецепта", max_length=200)
     image = models.ImageField(
-        upload_to="media/images/",
+        upload_to="images/",
     )
     text = models.TextField("Описание рецепта")
     ingredients = models.ManyToManyField(
