a9c80d01d911fa7a9c8603baef362d7656d532e4
ragozindenis <Ragozin3Dm@yandex.ru>
2023-03-13 15:30:11 -0300

    test

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -139,7 +139,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static/admin")
 
 MEDIA_URL = "/media/"
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "mediafiles")
 
 AUTH_USER_MODEL = "users.User"
 
