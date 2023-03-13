f6243487f132ec058d45d76c103b849277c7146d
ragozindenis <Ragozin3Dm@yandex.ru>
2023-03-12 21:12:52 -0300

    test

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,5 +1,3 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.urls import include, path
 from rest_framework import routers
 
@@ -15,8 +13,3 @@ urlpatterns = [
     path("", include(router_v1.urls)),
     path("auth/", include("djoser.urls.authtoken")),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
-    )
