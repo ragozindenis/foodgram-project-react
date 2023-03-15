c95780d3abbf522eecdbb4725291e3fe0c7e0d96
ragozindenis <Ragozin3Dm@yandex.ru>
2023-03-15 15:32:06 -0300

    testt

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv("SECRET_KEY", default="key_secret")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv["DEBUG_VALUE"] == "False"
 
 ALLOWED_HOSTS = os.getenv("SERVERNAMES", "127.0.0.1 localhost").split()
 
