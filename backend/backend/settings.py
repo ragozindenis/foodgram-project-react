3d8a0e90e26cd6fb1ecfea0a1af199b66efdb9c4
ragozindenis <Ragozin3Dm@yandex.ru>
2023-03-16 19:16:20 -0300

    test

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv("SECRET_KEY", default="key_secret")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = os.getenv("SERVERNAMES", "127.0.0.1 localhost").split()
 
@@ -170,3 +170,5 @@ DJOSER = {
 }
 
 USE_X_FORWARDED_HOST = True
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
